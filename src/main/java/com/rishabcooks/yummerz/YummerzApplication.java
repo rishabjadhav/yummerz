5aeb1fd673dd912330aed9a8e2a0a4d4abec4820
Rishab Jadhav <rishabjadhav@Rishabs-Air.attlocal.net>
2025-05-17 01:21:13 -0500

    lets get it started in here

---
+++ src/main/java/com/rishabcooks/yummerz/YummerzApplication.java
@@ -0,0 +1,13 @@
+package com.rishabcooks.yummerz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YummerzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YummerzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rishabcooks/yummerz/YummerzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rishabcooks.yummerz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YummerzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
